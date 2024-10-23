dd0cc310b17664ca644cea87941b27b3e0966f33
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2024-10-23 17:38:32 +0200

    DEV UI5InputMarkdownEditor basic functionality restored

--- Facades/Elements/UI5InputMarkdown.php
+++ Facades/Elements/UI5InputMarkdown.php
@@ -7,6 +7,25 @@ use exface\Core\Facades\AbstractAjaxFacade\Elements\ToastUIEditorTrait;
 class UI5InputMarkdown extends UI5Input
 {
     use ToastUIEditorTrait;
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Text::buildJsConstructorForMainControl()
+     */
+    public function buildJsConstructorForMainControl($oControllerJs = 'oController')
+    {
+        $this->registerExternalModules($this->getController());
+        return <<<JS
+
+        new sap.ui.core.HTML("{$this->getId()}", {
+            content: {$this->escapeString($this->buildHtmlMarkdownEditor())},
+            afterRendering: function(oEvent) {
+                var {$this->buildJsMarkdownVar()} = {$this->buildJsMarkdownInitEditor()}
+            }
+        })
+JS;
+    }
 
     /**
      *
@@ -16,8 +35,31 @@ class UI5InputMarkdown extends UI5Input
     public function registerExternalModules(\exface\UI5Facade\Facades\Interfaces\UI5ControllerInterface $controller) : UI5AbstractElement
     {
         $controller->addExternalModule('libs.exface.custom.toastUi', $this->getFacade()->buildUrlToSource('LIBS.TOASTUI.EDITOR.JS'), 'toastui');
-        $controller->addExternalModule('libs.exface.custom.mermaid', $this->getFacade()->buildUrlToSource('LIBS.MERMAID.JS'), 'mermaid');
+        //$controller->addExternalModule('libs.exface.custom.mermaid', $this->getFacade()->buildUrlToSource('LIBS.MERMAID.JS'), 'mermaid');
         $controller->addExternalCss('vendor/npm-asset/toast-ui--editor/dist/toastui-editor.css');
         return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\UI5Facade\Facades\Elements\UI5Input::getHeight()
+     */
+    public function getHeight()
+    {
+        if ($this->getWidget()->getHeight()->isUndefined()) {
+            return (2 * $this->getHeightRelativeUnit()) . 'px';
+        }
+        return parent::getHeight();
+    }
+
+    public function buildJsDataGetter(\exface\Core\Interfaces\Actions\ActionInterface $action = null)
+    {
+        $a = 0;
+        if ($this->getWidget()->isDisplayOnly()) {
+            return '{}';
+        } else {
+            return parent::buildJsDataGetter($action);
+        }
     }
 }
