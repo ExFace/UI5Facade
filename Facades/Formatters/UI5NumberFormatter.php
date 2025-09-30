591e28f086e3c223f58201cafa7475ec0d3134a2
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2025-09-30 16:11:19 +0200

    FIX UI5NumberFormatter removed redundant formatter

--- Facades/Formatters/UI5NumberFormatter.php
+++ Facades/Formatters/UI5NumberFormatter.php
@@ -56,49 +56,7 @@ JS;
 JS;
         }
 
-        // Add a custom formatter if required for
-        // - prefix/suffix
-        // - `+`-sign
-        // TODO handle NumberDataType::getEmptyFormat() here too?
-        if (($type instanceof NumberDataType) 
-            && (
-                $type->getPrefix() !== null
-                || $type->getSuffix() !== null
-                || $type->getShowPlusSign() === true
-            )
-        ) {
-            $prefix = $type->getPrefix();
-            $prefixJs = $prefix === '' || $prefix === null ? '""' : json_encode($prefix . ' ');
-            $suffix = $type->getSuffix();
-            $suffixJs = $suffix === '' || $suffix === null ? '""' : json_encode(' ' . $suffix);
-            $plusSignJs = $type->getShowPlusSign() ? 'true' : 'false';
-            
-            $otherProps .= <<<JS
-
-                formatter: function(mVal) {
-                    mVal = ({$this->getJsFormatter()->buildJsFormatter('mVal')});
-                    var sPrefix = $prefixJs;
-                    var sSuffix = $suffixJs;
-                    var bPlusSign = $plusSignJs;
-                    var nVal = {$this->getJsFormatter()->buildJsFormatParser('mVal')};
-
-                    if (mVal === '' || mVal === null || mVal === undefined) return mVal;
-
-                    if (bPlusSign === true && nVal > 0) {
-                        mVal = '+' + mVal;
-                    }
-
-                    if (sPrefix !== '') {
-                        mVal = sPrefix + mVal;
-                    }
-                    if (sSuffix !== '') {
-                        mVal = mVal + sSuffix;
-                    }                    
-
-                    return mVal;
-                },
-JS;
-        } else if ($type instanceof NumberDataType) {
+        if($type instanceof NumberDataType) {
             $otherProps .= <<<JS
                 formatter: function(mVal){
                     return ({$this->getJsFormatter()->buildJsFormatter('mVal')});
