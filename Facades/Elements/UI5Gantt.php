52184c8614a91c7cf78d9d6e79309ddba18a832d
Sergej Riel <sergej.riel@partner.bmwgroup.com>
2025-11-03 10:03:13 +0100

    Feat: Added new translations for the gantt chard view modes

--- Facades/Elements/UI5Gantt.php
+++ Facades/Elements/UI5Gantt.php
@@ -580,7 +580,7 @@ JS;
 
         foreach ($viewModes as $viewMode) {
             $buttons[] = [
-                'caption' => $viewMode,
+                'caption' => $this->translateViewMode($viewMode),
                 'action'  => [
                     'alias'  => 'exface.Core.CustomFacadeScript',
                     'hide_icon' => true,
@@ -613,5 +613,20 @@ JS
         }
         
         return $viewMode;
+    }
+    
+    protected function translateViewMode($viewMode) : string
+    {
+        $translator = $this->getWidget()->getWorkbench()->getCoreApp()->getTranslator();
+        
+        switch ($viewMode) {
+            case 'Quater Day': $translation = $translator->translate('WIDGET.GANTT_CHARD.VIEW_MODE_QUARTER_DAY'); break;
+            case 'Day': $translation = $translator->translate('WIDGET.GANTT_CHARD.VIEW_MODE_DAY'); break;
+            case 'Month': $translation = $translator->translate('WIDGET.GANTT_CHARD.VIEW_MODE_MONTH'); break;
+            case 'Week': $translation = $translator->translate('WIDGET.GANTT_CHARD.VIEW_MODE_WEEK'); break;
+            case 'Year': $translation = $translator->translate('WIDGET.GANTT_CHARD.VIEW_MODE_YEAR'); break;
+            default: $translation = $viewMode; break;
+        }
+        return $translation;
     }
 }
