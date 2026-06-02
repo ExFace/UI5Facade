de061ee196554c480b2a261040f19849781e99ba
Saskia Hustinx <91604850+sHustinx@users.noreply.github.com>
2026-06-02 20:59:29 +0200

    FIX MenuButton: added support for disabled_if on parent button (#356)

--- Facades/Elements/UI5MenuButton.php
+++ Facades/Elements/UI5MenuButton.php
@@ -179,4 +179,18 @@ JS);
         
         return parent::registerConditionalProperties();
     }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \exface\Core\Facades\AbstractAjaxFacade\Elements\AbstractJqueryElement::buildJsSetDisabled()
+     */
+    public function buildJsSetDisabled(bool $trueOrFalse) : string
+    {
+        if ($trueOrFalse === true) {
+            return "sap.ui.getCore().byId('{$this->getId()}')?.setEnabled(false);";
+        } else {
+            return "sap.ui.getCore().byId('{$this->getId()}')?.setEnabled(true);";
+        }
+    }
 }
